e00bb97d120c4c834cd188243a5e73bfc7d1e4ac
DanielMiskowiec <mrdami99@gmail.com>
2023-03-13 12:56:03 +0100

    1 zrobione

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const itemElement = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemElement.length} `);
+itemElement.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
