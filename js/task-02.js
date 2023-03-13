fe86768bf98904f864760ab96d8e167a04f4f137
DanielMiskowiec <mrdami99@gmail.com>
2023-03-13 13:26:51 +0100

    zadanie 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (let i = 0; i < ingredients.length; i+= 1) {
+  const name = ingredients[i];
+  const itemElement = document.createElement("li");
+  itemElement.classList.add("item");
+  itemElement.textContent = `${ name }`;
+
+  const listElement = document.getElementById("ingredients");
+  listElement.append(itemElement);
+
+}
+
+
+
