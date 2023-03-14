3fad5da3fe1996bad96b489a352e8330259f3cb1
DanielMiskowiec <mrdami99@gmail.com>
2023-03-14 11:23:35 +0100

    zadanie 5

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-input'),
+};
+
+refs.input.addEventListener('input', onChange);
+function onChange(event) {
+    refs.span.textContent = event.target.value;
+}
