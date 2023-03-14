b66acc8d6cab77951aaac6e4103ede7247a0acb3
DanielMiskowiec <mrdami99@gmail.com>
2023-03-14 19:17:24 +0100

    zadanie 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const emailElement = document.querySelector(".login-form");
+emailElement.addEventListener("submit", emailElementSubmit);
 
+function emailElementSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fiellds!");
+    }
+
+    const userAccount = { email: email.value, password: password.value };
+    console.log("userAccount", userAccount);
+
+    event.currentTarget.reset();
+}
